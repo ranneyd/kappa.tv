2edbe55ac915fc91c0f0a6ee68f1059896058034
Sean Reimer <sreimer15@gmail.com>
2015-11-19 10:56:38 -0600

    Adds ToolTip Functionality to "Hamburger": when hovering the channel name tooltip appears on the right. (not positive on aesthetic value, does not use info-title css anymore)

--- js/main.js
+++ js/main.js
@@ -222,8 +222,14 @@ function updateInfo(){
 	// Even if a stream is offline, this info is still available
 	$.get("https://api.twitch.tv/kraken/channels/"+global_channel, function( data ){
 		$("img.logo").attr("src", data.logo);
-		var title = global_channel + " playing " + data.game;
-		$("#info-title > span").html(title).attr('title', title);
+		var title = " Playing " + data.game;
+		$("#info-title > span").html(global_channel).attr({
+													'title': title,
+													'data-toggle': 'tooltip',
+													'data-placement': 'right'
+													}).tooltip('toggle');
+		// $("#info-title > span").html(<button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="right" title="...">...</button>)
+		// $("#info-title > span").tooltip('toggle');
 		$("#stream-title > span").html(data.status);
 		$("#total-views > span").html(data.views);
 		$("#followers > span").html(data.followers);
